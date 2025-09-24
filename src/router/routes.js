6b09ff7f84aabe40641b5bd5bc9319d42d2554d1
Daria Steblovska <darias1896.96@gmail.com>
2025-09-24 20:25:37 +0100

    routes to main pages added

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,33 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        path: 'books',
+        name: 'books',
+        component: () => import('pages/books/BooksCollections.vue'),
+      },
+      {
+        path: 'add-book',
+        name: 'addBook',
+        component: () => import('pages/AddBookPage.vue'),
+      },
+      {
+        path: 'goals',
+        name: 'goals',
+        component: () => import('pages/goals/GoalsPage.vue'),
+      },
+      {
+        path: 'stats',
+        name: 'stats',
+        component: () => import('pages/StatisticsPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
