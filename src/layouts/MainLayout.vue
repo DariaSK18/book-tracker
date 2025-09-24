d5f661d6b1596c3a5f3c447dad53d07b7fe1df66
Daria Steblovska <darias1896.96@gmail.com>
2025-09-24 03:52:16 +0100

    modified layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,16 +3,13 @@
     <q-header elevated>
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title>MR. Bookich</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header>Your collections</q-item-label>
 
         <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
@@ -21,6 +18,9 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer>
+      footer
+    </q-footer>
   </q-layout>
 </template>
 
@@ -30,46 +30,28 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
+    title: 'Add single book',
     caption: 'quasar.dev',
-    icon: 'school',
+    icon: 'book',
     link: 'https://quasar.dev',
   },
   {
-    title: 'Github',
+    title: 'Add multiple books',
     caption: 'github.com/quasarframework',
-    icon: 'code',
+    icon: 'book',
     link: 'https://github.com/quasarframework',
   },
   {
-    title: 'Discord Chat Channel',
+    title: 'Discover books',
     caption: 'chat.quasar.dev',
-    icon: 'chat',
+    icon: 'search',
     link: 'https://chat.quasar.dev',
   },
   {
-    title: 'Forum',
+    title: 'Goodreads import (pro)',
     caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
+    icon: 'download',
     link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
   },
 ]
 
