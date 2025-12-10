e7335132ac0884dff06926ab50bc47f7d2e656dc
Daria Steblovska <darias1896.96@gmail.com>
2025-12-10 22:15:08 +0000

    back: associations added between book-user, book-note, user-note

--- server/models/index.mjs
+++ server/models/index.mjs
@@ -1,108 +1,38 @@
-// import Book from "./book.mjs";
-// import User from "./user.mjs";
-// import Note from "./note.mjs";
+import Book from "./book.mjs";
+import User from "./user.mjs";
+import Note from "./note.mjs";
 
-// Meme.belongsTo(User, {
-//   foreignKey: "user_id",
-//   as: "user",
-// });
+Book.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "user",
+});
 
-// User.hasMany(Meme, {
-//   foreignKey: "user_id",
-//   as: "memes",
-// });
+User.hasMany(Book, {
+  foreignKey: "user_id",
+  as: "books",
+  onDelete: "CASCADE",
+});
 
-// Meme.hasMany(Comment, {
-//   foreignKey: "meme_id",
-//   as: "comments",
-// });
+Book.hasMany(Note, {
+  foreignKey: "book_id",
+  as: "notes",
+  onDelete: "CASCADE",
+});
 
-// Comment.belongsTo(Meme, {
-//   foreignKey: "meme_id",
-//   as: "meme",
-// });
+Note.belongsTo(Book, {
+  foreignKey: "book_id",
+  as: "book",
+});
 
+User.hasMany(Note, {
+  foreignKey: "user_id",
+  as: "notes",
+  onDelete: "CASCADE",
+});
 
-// User.hasMany(Comment, {
-//   foreignKey: "user_id",
-//   as: "comments",
-// });
+Note.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "user",
+});
 
-// Comment.belongsTo(User, {
-//   foreignKey: "user_id",
-//   as: "user",
-// });
-
-// User.hasMany(Like, {
-//   foreignKey: "user_id",
-//   as: "likes",
-// });
-
-// Meme.hasMany(Like, {
-//   foreignKey: "meme_id",
-//   as: "likes",
-// });
-
-// Like.belongsTo(User, {
-//   foreignKey: "user_id",
-//   as: "user",
-// });
-
-// Like.belongsTo(Meme, {
-//   foreignKey: "meme_id",
-//   as: "meme",
-// });
-
-// Meme.belongsToMany(Tag, {
-//   through: MemeTag,
-//   foreignKey: "meme_id",
-//   otherKey: 'tag_id',
-//   as: "tags",
-// });
-
-// Tag.belongsToMany(Meme, {
-//   through: MemeTag,
-//   foreignKey: "tag_id",
-//   otherKey: 'meme_id',
-//   as: "memes",
-// });
-
-// // MemeTag.belongsTo(Meme, {
-// //   foreignKey: "meme_id",
-// //   as: "meme",
-// // });
-
-// // MemeTag.belongsTo(Tag, {
-// //   foreignKey: "tag_id",
-// //   as: "tag",
-// // });
-
-// Follow.belongsTo(User, {
-//   foreignKey: "follower_id",
-//   as: "follower",
-// });
-
-// Follow.belongsTo(User, {
-//   foreignKey: "following_id",
-//   as: "following",
-// });
-
-// User.hasMany(Follow, {
-//   foreignKey: "follower_id",
-//   as: "following",
-// });
-
-// User.hasMany(Follow, {
-//   foreignKey: "following_id",
-//   as: "followers",
-// });
-
-// export {
-//   Meme,
-//   User,
-//   Comment,
-//   Like,
-//   Tag,
-//   Follow,
-//   MemeTag,
-// };
+export { Book, User, Note };
