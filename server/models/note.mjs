d34b9c2165b882e9885849ba0c3be1958237d45d
Daria Steblovska <darias1896.96@gmail.com>
2025-12-10 21:56:58 +0000

    back: note model added

---
+++ server/models/note.mjs
@@ -0,0 +1,44 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/connection.mjs";
+
+class Note extends Model {}
+
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    book_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "book",
+        key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "note",
+  }
+);
+
+export default Note;
