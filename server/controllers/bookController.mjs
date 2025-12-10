a1786b442d60047f6937642fb68c3509fea3f185
Daria Steblovska <darias1896.96@gmail.com>
2025-12-10 23:59:36 +0000

    back: book controller created (get all books only)

---
+++ server/controllers/bookController.mjs
@@ -0,0 +1,224 @@
+import { catchAsync } from "../utils/catchAsync.mjs";
+// import AppError from "../utils/AppError.mjs";
+import Book from "../models/book.mjs";
+// import User from "../models/user.mjs";
+import { sendResponse } from "../utils/helpers/sendResponse.mjs";
+// import { Op } from "sequelize";
+
+// --- get all posts ---
+export const getAllPosts = catchAsync(async (req, res, next) => {
+  const books = await Book.findAll();
+  sendResponse(res, 200, { books });
+});
+
+// export const getAllPosts = catchAsync(async (req, res, next) => {
+//   const page = parseInt(req.query.page) || 1;
+//   const limit = parseInt(req.query.limit) || 10;
+//   const { search, category } = req.query;
+
+//   const where = {};
+
+//   if (category) where.category = category;
+
+//   let searchCondition = [];
+
+//   if (search) {
+//     searchCondition.push({ title: { [Op.like]: `%${search}%` } });
+
+//     const taggedPosts = await Meme.findAll({
+//       include: [
+//         {
+//           model: Tag,
+//           as: "tags",
+//           where: { tag_name: { [Op.like]: `%${search}%` } },
+//           attributes: [],
+//           through: { attributes: [] },
+//           required: true,
+//         },
+//       ],
+//       attributes: ["id"],
+//     });
+//     if (taggedPosts.length > 0) {
+//       const postIds = taggedPosts.map((p) => p.id);
+//       searchCondition.push({ id: { [Op.in]: postIds } });
+//     }
+//     if (searchCondition.length > 0) {
+//       where[Op.or] = searchCondition;
+//     }
+//   }
+//   const { count: totalPosts, rows: posts } = await Meme.findAndCountAll({
+//     where,
+//     include: [
+//       { model: User, as: "user", attributes: ["id", "username"] },
+//       { model: Like, as: "likes", attributes: ["id"] },
+//       { model: Comment, as: "comments", attributes: ["id"] },
+//       { model: Tag, as: "tags", through: { attributes: [] } },
+//     ],
+//     order: [["createdAt", "DESC"]],
+//     limit,
+//     offset: (page - 1) * limit,
+//     distinct: true,
+//   });
+
+//   const postsWithCounts = posts.map((post) => {
+//     const p = post.toJSON();
+//     return {
+//       ...p,
+//       likesCount: p.likes.length,
+//       commentsCount: p.comments.length,
+//     };
+//   });
+
+//   const totalPages = Math.ceil(totalPosts / limit);
+
+//   sendResponse(res, 200, {
+//     posts: postsWithCounts,
+//     pagination: {
+//       currentPage: page,
+//       totalPages,
+//     },
+//   });
+// });
+
+// // --- create a post ---
+
+// export const createPost = catchAsync(async (req, res, next) => {
+//   const {
+//     user,
+//     body: { title, description, category, image_url, tags },
+//   } = req;
+//   if (!title || !description)
+//     return next(new AppError("Title and description are required", 400));
+
+//   const post = await Meme.create({
+//     title,
+//     description,
+//     category,
+//     image_url,
+//     user_id: user.id,
+//   });
+
+//   if (tags && Array.isArray(tags) && tags.length) {
+//     for (let tagName of tags) {
+//       const [tag] = await Tag.findOrCreate({
+//         where: { tag_name: tagName },
+//       });
+
+//       await MemeTag.findOrCreate({
+//         where: {
+//           meme_id: post.id,
+//           tag_id: tag.id,
+//         },
+//       });
+//     }
+//   }
+//   sendResponse(res, 201, post);
+// });
+
+// // --- get one post by id ---
+
+// export const getOnePost = catchAsync(async (req, res, next) => {
+//   const {
+//     params: { id },
+//   } = req;
+//   const post = await Meme.findByPk(id, {
+//     include: {
+//       model: Tag,
+//       as: "tags",
+//       through: { attributes: [] },
+//     },
+//   });
+//   if (!post) return next(new AppError("Post not found", 404));
+//   sendResponse(res, 200, post);
+// });
+
+// // --- update a field ---
+
+// export const updatePost = catchAsync(async (req, res, next) => {
+//   const {
+//     params: { id },
+//     body: { title, description, category, image_url, tags },
+//     user,
+//   } = req;
+
+//   const post = await Meme.findByPk(id);
+//   if (!post) return next(new AppError("Post not found", 404));
+//   if (post.user_id !== user.id)
+//     return next(new AppError("Not the author", 403));
+
+//   if (title) post.title = title;
+//   if (description) post.description = description;
+//   if (category) post.category = category;
+//   if (image_url) post.image_url = image_url;
+
+//   if (tags && Array.isArray(tags)) {
+//     await MemeTag.destroy({ where: { meme_id: post.id } });
+
+//     for (const tagName of tags) {
+//       const [tag] = await Tag.findOrCreate({
+//         where: { tag_name: tagName },
+//       });
+//       await MemeTag.findOrCreate({
+//         where: { meme_id: post.id, tag_id: tag.id },
+//       });
+//     }
+//   }
+
+//   const updated = await post.save();
+//   sendResponse(res, 200, updated);
+// });
+
+// // --- delete post by id ---
+
+// export const deletePost = catchAsync(async (req, res, next) => {
+//   const {
+//     params: { id },
+//     user,
+//   } = req;
+
+//   const post = await Meme.findByPk(id);
+//   if (!post) return next(new AppError("Post not found", 404));
+
+//   if (post.user_id !== user.id)
+//     return next(new AppError("Not the author", 403));
+
+//   await MemeTag.destroy({ where: { meme_id: id } });
+//   await post.destroy();
+
+//   sendResponse(res, 200, { msg: "Post deleted successfully" });
+// });
+
+// // --- toggle like for post ---
+
+// export const toggleLike = catchAsync(async (req, res, next) => {
+//   const postId = req.params.id;
+//   const userId = req.user.id;
+
+//   const post = await Meme.findByPk(postId);
+//   if (!post) return next(new AppError("Post not found", 404));
+
+//   const existingLike = await Like.findOne({
+//     where: { user_id: userId, meme_id: postId },
+//   });
+
+//   let liked;
+
+//   if (existingLike) {
+//     await existingLike.destroy();
+//     liked = false;
+//   } else {
+//     await Like.create({
+//       user_id: userId,
+//       meme_id: postId,
+//     });
+//     liked = true;
+//   }
+
+//   const likeCount = await Like.count({
+//     where: { meme_id: postId },
+//   });
+//   sendResponse(res, 200, {
+//     likes: likeCount,
+//     liked: liked,
+//   });
+// });
