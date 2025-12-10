952c9c9439eb35241660ae7fd8c001924c238721
Daria Steblovska <darias1896.96@gmail.com>
2025-12-10 22:30:54 +0000

    back: category model created

---
+++ server/models/category.mjs
@@ -0,0 +1,29 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/connection.mjs";
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+export default Category;
