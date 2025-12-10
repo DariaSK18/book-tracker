449cb8e49e726a90bd0cd016a5c04420e2ff19e9
Daria Steblovska <darias1896.96@gmail.com>
2025-12-10 22:52:17 +0000

    back: sequelize added to server

--- server/server.mjs
+++ server/server.mjs
@@ -1,9 +1,13 @@
 import app from "./app.mjs";
+import sequelize from "./config/connection.mjs";
+import "./models/index.mjs";
 
 const PORT = process.env.PORT || 3000;
 const HOST = "0.0.0.0";
 
-app.listen(PORT, () => {
-  console.log(`Running on PORT ${PORT}`);
-  console.log(`http://${HOST}:${PORT}`);
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Running on PORT ${PORT}`);
+    console.log(`http://${HOST}:${PORT}`);
+  });
 });
