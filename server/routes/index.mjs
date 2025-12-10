f8013eaaefd5c9cc926db32d2a09f1ed7f244da5
Daria Steblovska <darias1896.96@gmail.com>
2025-12-10 23:27:27 +0000

    back: routes file created to import all routes

---
+++ server/routes/index.mjs
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import userRoutes from "./userRoutes.mjs";
+import bookRoutes from "./bookRoutes.mjs";
+
+const router = Router()
+
+router.use('/user', userRoutes)
+router.use('/book', postRoutes)
+
+export default router
