88969d2741439f937bf42ddae5b960c9c23ce6ca
Daria Steblovska <darias1896.96@gmail.com>
2025-12-11 20:48:11 +0000

    front: testing backgrounds with react bits

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -3,12 +3,33 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 // import { useAlert } from "../context/AlertContext";
 // import Alert from "../components/Alert";
+// import LiquidEther from '../component/LiquidEther';
+// import "../App.css"
 
 export default function MainLayout() {
 //   const { alert, clearAlert } = useAlert();
 
   return (
     <div className="wrapper">
+      {/* <div className="bg-st" style={{ width: '100%', height: 600, position: 'relative' }}>
+  <LiquidEther
+    colors={[ '#29ff4c', '#a0ff9e', '#f0eaa3' ]}
+    mouseForce={10}
+    cursorSize={100}
+    isViscous={false}
+    viscous={30}
+    iterationsViscous={32}
+    iterationsPoisson={32}
+    resolution={0.5}
+    isBounce={false}
+    autoDemo={true}
+    autoSpeed={0.5}
+    autoIntensity={2.2}
+    takeoverDuration={0.25}
+    autoResumeDelay={3000}
+    autoRampDuration={0.6}
+  /></div> */}
+
       <Header />
       {/* {alert && (
         <Alert
@@ -22,6 +43,7 @@ export default function MainLayout() {
         <div className="main__container"><Outlet /></div>
       </main>
       <Footer />
-    </div>
+      </div>
+    
   );
 }
