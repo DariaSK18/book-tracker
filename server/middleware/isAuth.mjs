f516d95ba3f2e0e20da830141f87102957f6980c
Daria Steblovska <darias1896.96@gmail.com>
2025-12-11 00:31:05 +0000

    back: middleware updated to check if user is author of the posted book

--- server/middleware/isAuth.mjs
+++ server/middleware/isAuth.mjs
@@ -1,4 +1,5 @@
 import AppError from "../utils/AppError.mjs";
+import Book from "../models/book.mjs";
 
 export const isAuth = (req, res, next) => {
   if (req.isAuthenticated()) return next();
@@ -9,5 +10,19 @@ export const isAuth = (req, res, next) => {
 
 export const isUser = (req, res, next) => {
   res.locals.user = req.user || null;
+  next();
+};
+
+export const isAuthor = async (req, res, next) => {
+  const {
+    params: { id },
+    user,
+  } = req;
+  if (!user.id) return next(new AppError("Not authenticated", 401));
+
+  const book = await Book.findByPk(id);
+  if (!book) return next(new AppError("Book not found", 404));
+  if (book.user_id.toString() !== user.id.toString())
+    return next(new AppError("Not the author of the posted book", 403));
   next();
 };
