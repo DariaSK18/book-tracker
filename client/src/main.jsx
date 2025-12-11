21a14a30a6f1c021be9e79563f50d99999b98016
Daria Steblovska <darias1896.96@gmail.com>
2025-12-11 23:59:02 +0000

    front: links updated for styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 // import './index.css'
 import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
+import './styles/styles.scss';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
