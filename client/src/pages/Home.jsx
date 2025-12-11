ae7bcc8aa991d0db8a183227687c3b864abf7d1f
Daria Steblovska <darias1896.96@gmail.com>
2025-12-11 14:34:51 +0000

    front: displaying title of the books to test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
+import { getAllBooks } from "../api/booksApi";
 export default function Home() {
+const [books, setBooks] = useState([])
+
+ useEffect(() => {
+    async function fetchBooks() {
+      try {
+        const res = await getAllBooks();
+        setBooks(res.data.books);
+        // console.log(books.data);
+        
+      } catch (err) {
+        console.error("Failed to load books:", err);
+      }
+    }
+
+    fetchBooks();
+  }, []);
 
   return (
     <div className="home">
-      Home
+      Home  {books.map(book=>(<p key={book.id}>{book.title}</p>))}
     </div>
   );
 }
