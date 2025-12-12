b533de96c6a795927339824bc2a19e859842b07f
Daria Steblovska <darias1896.96@gmail.com>
2025-12-12 21:20:35 +0000

    front: upload book page book image changed to url for now

--- client/src/pages/UploadBook.jsx
+++ client/src/pages/UploadBook.jsx
@@ -14,8 +14,9 @@ export default function UploadBook() {
     collection: "",
     genre: "",
     readingStatus: "",
-    image: null,
-    urlImg: null,
+    // image: null,
+    // urlImg: null,
+    image_url: "",
   });
 
   const readingStatusOptions = [
@@ -24,16 +25,16 @@ export default function UploadBook() {
     { label: "Book I'm going to read", value: "will" },
   ];
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setBookData((prev) => ({
-        ...prev,
-        image: file,
-        urlImg: URL.createObjectURL(file),
-      }));
-    }
-  };
+  // const handleFileChange = (e) => {
+  //   const file = e.target.files[0];
+  //   if (file) {
+  //     setBookData((prev) => ({
+  //       ...prev,
+  //       image: file,
+  //       urlImg: URL.createObjectURL(file),
+  //     }));
+  //   }
+  // };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -52,6 +53,7 @@ export default function UploadBook() {
         collection: bookData.collection,
         genre: bookData.genre,
         reading_status: bookData.readingStatus,
+        image_url: bookData.image_url,
       };
 
       await createBook(payload);
@@ -67,85 +69,119 @@ export default function UploadBook() {
     <div>
       <h4>Add Book</h4>
 
-      <form onSubmit={handleSubmit}>
-        <div>
+      <form onSubmit={handleSubmit} className="book-form">
+        <div className="top-btns">
           <button type="button">Search Online</button>
           <button type="button">Scan ISBN code</button>
         </div>
         <p>or add manually</p>
+        <div className="form-grid">
+          <div className="form-inputs">
+            <input
+              type="text"
+              placeholder="Book title *"
+              name="title"
+              value={bookData.title}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Book author *"
+              name="author"
+              value={bookData.author}
+              onChange={handleChange}
+            />
+            <textarea
+              placeholder="Description *"
+              name="description"
+              value={bookData.description}
+              onChange={handleChange}
+              rows={4}
+              style={{ resize: "vertical" }}
+            />
+            <input
+              type="number"
+              placeholder="Number of pages *"
+              name="numOfPages"
+              value={bookData.numOfPages}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Collection"
+              name="collection"
+              value={bookData.collection}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Genre"
+              name="genre"
+              value={bookData.genre}
+              onChange={handleChange}
+            />
+          </div>
 
-        <input type="file" accept="image/*" onChange={handleFileChange} />
-        {bookData.urlImg && (
-          <img
-            src={bookData.urlImg}
-            alt="Preview"
-            style={{ maxWidth: "400px"}}
-          />
-        )}
-
-        <input
-          type="text"
-          placeholder="Book title *"
-          name="title"
-          value={bookData.title}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Book author *"
-          name="author"
-          value={bookData.author}
-          onChange={handleChange}
-        />
-        <textarea
-          placeholder="Description *"
-          name="description"
-          value={bookData.description}
-          onChange={handleChange}
-          rows={4}
-          style={{ resize: "vertical" }}
-        />
-        <input
-          type="number"
-          placeholder="Number of pages *"
-          name="numOfPages"
-          value={bookData.numOfPages}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Collection"
-          name="collection"
-          value={bookData.collection}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Genre"
-          name="genre"
-          value={bookData.genre}
-          onChange={handleChange}
-        />
-
-        <div>
-          {readingStatusOptions.map((option) => (
-            <label
-              key={option.value}>
+          <div className="form-extra">
+            {/* <div className="radio-group">
+              {readingStatusOptions.map((option) => (
+                <label
+                  key={option.value}>
+                  <input
+                    type="radio"
+                    name="readingStatus"
+                    value={option.value}
+                    checked={bookData.readingStatus === option.value}
+                    onChange={handleChange}
+                  />{" "}
+                  <span className="custom-radio"></span>
+      <span className="radio-label">{option.label}</span>
+                </label>
+              ))}
+            </div> */}
+            <div className="radio-group">
+              {readingStatusOptions.map((option) => (
+                <label className="radio" key={option.value}>
+                  <input
+                    type="radio"
+                    name="readingStatus"
+                    value={option.value}
+                    checked={bookData.readingStatus === option.value}
+                    onChange={handleChange}
+                  />
+                  <span className="custom-radio"></span>
+                  <span className="radio-label">{option.label}</span>
+                </label>
+              ))}
+            </div>
+            <div className="form-extra__img">
+              {/* <input type="file" accept="image/*" onChange={handleFileChange} /> */}
               <input
-                type="radio"
-                name="readingStatus"
-                value={option.value}
-                checked={bookData.readingStatus === option.value}
+                name="image_url"
+                type="url"
+                placeholder="Image URL"
+                value={bookData.image_url}
                 onChange={handleChange}
-              />{" "}
-              {option.label}
-            </label>
-          ))}
+              />
+              {bookData.image_url && (
+                <img
+                  src={bookData.image_url}
+                  alt="Preview"
+                  style={{ maxWidth: "400px" }}
+                />
+              )} 
+              {/* {bookData.urlImg && (
+                <img
+                  src={bookData.urlImg}
+                  alt="Preview"
+                  style={{ maxWidth: "400px" }}
+                />
+              )} */}
+            </div>
+          </div>
         </div>
 
-        <button type="submit">
-          Add Book
-        </button>
+        <button type="submit">Add Book</button>
       </form>
 
       {/* <pre>{JSON.stringify(bookData, null, 2)}</pre> */}
