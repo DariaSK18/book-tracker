f57ae1ed7f1f4a93be11f2260df32007eec5184a
Daria Steblovska <darias1896.96@gmail.com>
2025-12-16 02:58:06 +0000

    front: tested react bits components

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,8 +1,35 @@
+import Folder from '../component/Folder'
+import GlassIcons from '../component/GlassIcons'
+import { 
+  FiFileText, 
+  FiBook, 
+  FiHeart, 
+  FiCloud, 
+  FiEdit, 
+  FiBarChart2 
+} from 'react-icons/fi';
+
+
 export default function Categories() {
+
+  const items = [
+  { icon: <FiFileText />, color: 'blue', label: 'Files' },
+  { icon: <FiBook />, color: 'purple', label: 'Books' },
+  { icon: <FiHeart />, color: 'red', label: 'Health' },
+  { icon: <FiCloud />, color: 'indigo', label: 'Weather' },
+  { icon: <FiEdit />, color: 'orange', label: 'Notes' },
+  { icon: <FiBarChart2 />, color: 'green', label: 'Stats' },
+];
 
   return (
     <div className="categories">
       Categories
+      {/* <div style={{ width: '100%', height: '100px', position: 'relative' }}>
+  <Folder size={1} color="#274fffff" className="custom-folder" />
+</div> */}
+<div style={{ width: '100%', height: '400px', position: 'absolute' }}>
+  <GlassIcons items={items} className="custom-class"/>
+</div>
     </div>
   );
 }
