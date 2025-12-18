a13741066d85a36828fd845dbf4426b84df65b73
Daria Steblovska <darias1896.96@gmail.com>
2025-12-18 04:24:29 +0000

    front: display books and navigation added

--- client/src/pages/CollectionBooks.jsx
+++ client/src/pages/CollectionBooks.jsx
@@ -1,13 +1,34 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getBooksByCollection } from "../api/booksApi";
 
 export default function CollectionBooks() {
-const { slug } = useParams();
-  console.log(slug);
+  const { slug } = useParams();
+  const collectionName = decodeURIComponent(slug);
+  const [books, setBooks] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchBooks() {
+      const res = await getBooksByCollection(collectionName);
+      setBooks(res.data.books);
+    }
+
+    fetchBooks();
+  }, [collectionName]);
 
   return (
     <div className="collection-books">
-      CollectionBooks <br/>
+      CollectionBooks <br />
       Books from {slug}
+      <h2>Books from collection: {slug}</h2>
+      {books.length === 0 && <p>No books in this collection</p>}
+      {books.map((book) => (
+        <div key={book.id} onClick={() => navigate(`/single-book/${book.id}`)}>
+          <h4>{book.title}</h4>
+          <p>{book.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
