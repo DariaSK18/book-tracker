f4022d855e1432da2c62c232f31a951de806604e
Daria Steblovska <darias1896.96@gmail.com>
2025-12-19 02:47:01 +0000

    front: collection book modified and imported book card to display

--- client/src/pages/CollectionBooks.jsx
+++ client/src/pages/CollectionBooks.jsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getBooksByCollection } from "../api/booksApi";
+import { getBooksByCollection, deleteBook } from "../api/booksApi";
+import BookCard from "../components/BookCard";
 
 export default function CollectionBooks() {
   const { slug } = useParams();
@@ -17,18 +18,33 @@ export default function CollectionBooks() {
     fetchBooks();
   }, [collectionName]);
 
+  async function handleDeleteBook(id) {
+  const confirmed = window.confirm("Are you sure you want to delete this book?");
+  if (!confirmed) return;
+
+  try {
+    await deleteBook(id);
+
+    setBooks((prev) => prev.filter((book) => book.id !== id));
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
   return (
     <div className="collection-books">
-      CollectionBooks <br />
-      Books from {slug}
-      <h2>Books from collection: {slug}</h2>
+      <h2 className="collection-books__title">Collection: {slug}</h2>
       {books.length === 0 && <p>No books in this collection</p>}
-      {books.map((book) => (
-        <div key={book.id} onClick={() => navigate(`/single-book/${book.id}`)}>
-          <h4>{book.title}</h4>
-          <p>{book.author}</p>
-        </div>
-      ))}
+      <div className="collection-books__wrapper">
+        {books.map((book) => (
+          
+          <div key={book.id} onClick={() => navigate(`/single-book/${book.id}`)}>
+            <BookCard data={book} onDelete={handleDeleteBook}/>
+            {/* <h4>{book.title}</h4>
+            <p>{book.author}</p> */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
