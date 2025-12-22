6f309346653b99756f908b6213e8d8906c357018
Daria Steblovska <darias1896.96@gmail.com>
2025-12-22 00:47:59 +0000

    back: reading logs and statistic routes imported to main file

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes.mjs";
 import bookRoutes from "./bookRoutes.mjs";
+import readingLogRoutes from "./readingLogRoutes.mjs";
+import statisticRoutes from "./statisticRoutes.mjs"
+
 
 const router = Router()
 
 router.use('/user', userRoutes)
 router.use('/book', bookRoutes)
+router.use('/reading-log', readingLogRoutes)
+router.use('/statistic', statisticRoutes)
 
 export default router
