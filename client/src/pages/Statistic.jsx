d8dfc6f84ff1e97d205b6d464bc4b3cff815a00e
Daria Steblovska <darias1896.96@gmail.com>
2025-12-22 04:52:17 +0000

    front: statistic component imported and used with data from backend in stats page

--- client/src/pages/Statistic.jsx
+++ client/src/pages/Statistic.jsx
@@ -1,8 +1,34 @@
+import StatisticCard from "../components/StatisticCard";
+import { useEffect, useState } from "react";
+import { getAllStats } from "../api/statsApi";
+
 export default function Statistic() {
+  const [stats, setStats] = useState(null);
+
+  useEffect(() => {
+    async function fetchStats() {
+          try {
+            const res = await getAllStats();
+            setStats(res.data.data);
+            // console.log(res.data.data);
+          } catch (err) {
+            console.error("Failed to load books:", err);
+          }
+        }
+    
+        fetchStats();
+  }, []);
+
+  // console.log(stats);
+  
+
+  if (!stats) return <div>Loading...</div>;
 
   return (
     <div className="statistic">
-      Statistic
+      <h3 className="statistic__title">Statistic</h3>
+
+      <StatisticCard data={stats}/>
     </div>
   );
 }
