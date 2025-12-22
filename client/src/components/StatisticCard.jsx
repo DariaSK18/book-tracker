d7bb64c8958b3d50a6f90af3bcd233c4882556bb
Daria Steblovska <darias1896.96@gmail.com>
2025-12-22 04:50:30 +0000

    front: statistic card component created and added

---
+++ client/src/components/StatisticCard.jsx
@@ -0,0 +1,62 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBook,
+  faClock,
+  faListOl,
+  faCalendarDay,
+  faTrophy,
+  faRocket,
+  faGauge,
+  faFire,
+} from "@fortawesome/free-solid-svg-icons";
+
+const iconsMap = {
+  pagesRead: faBook,
+  totalTime: faClock,
+  booksRead: faListOl,
+  totalReadingDays: faCalendarDay,
+  bestDayPages: faTrophy,
+  bestDayMinutes: faRocket,
+  readingSpeed: faGauge,
+  longestStreak: faFire,
+};
+
+const labelsMap = {
+  pagesRead: "Pages Read",
+  totalTime: "Total Time (min)",
+  booksRead: "Books Read",
+  totalReadingDays: "Reading Days",
+  bestDayPages: "Best Day Pages",
+  bestDayMinutes: "Best Day Minutes",
+  readingSpeed: "Reading Speed",
+  longestStreak: "Longest Streak",
+};
+
+const valuesMap = {
+  pagesRead: "pages",
+  totalTime: "minutes",
+  booksRead: "books",
+  totalReadingDays: "days",
+  bestDayPages: "pages",
+  bestDayMinutes: "minutes",
+  readingSpeed: "pages/h",
+  longestStreak: "days",
+};
+
+export default function StatisticCard({ data }) {
+    console.log('data', data);
+    
+  return (
+    <div className="statistic__statistic-card statistic-card">
+      {Object.keys(data).map((key) => (
+        <div className="statistic-card__item" key={key}>
+          <div className="statistic-card__icon">
+            <FontAwesomeIcon icon={iconsMap[key]} size="2x" />
+          </div>
+          <div className="statistic-card__text">{labelsMap[key]}</div>
+          <div className="statistic-card__values"><span className="statistic-card__nums">{data[key]}</span> {valuesMap[key]}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
