0ce55d4f61f096bd0cacb405acc3aada7790e104
Daria Steblovska <darias1896.96@gmail.com>
2025-12-29 05:34:07 +0000

    front: alert component used in main layout

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,25 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-// import { useAlert } from "../context/AlertContext";
-// import Alert from "../components/Alert";
 // import LiquidEther from '../component/LiquidEther';
 // import "../App.css"
+import { useAlert } from "../context/AlertContext";
+import Alert from "../components/Alert";
 
 export default function MainLayout() {
-  //   const { alert, clearAlert } = useAlert();
+    const { alert, clearAlert } = useAlert();
 
   return (
     <div className="wrapper">
       <Header />
-      {/* {alert && (
+      {alert && (
         <Alert
           type={alert.type}
           message={alert.message}
           onClose={clearAlert}
           fixed
         />
-      )} */}
+      )}
       <main className="main">
         <div className="main__container">
           <Outlet />
