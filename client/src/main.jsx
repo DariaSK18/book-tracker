f86dcad53beea3a7d2d9675df81f6a6ab797f862
Daria Steblovska <darias1896.96@gmail.com>
2025-12-29 05:31:19 +0000

    front: alert message connected to main file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 // import './index.css'
 import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
-import './styles/styles.scss';
+import "./styles/styles.scss";
+import { AlertProvider } from "./context/AlertContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <AlertProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </AlertProvider>
   </StrictMode>
 );
